4acbcabe4bcd69935b4e4d6926ea828f52b92e86
(nick) <nicholas.gordon@vtsd.com>
2024-12-09 09:24:40 -0500

    java test

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Challenge 3</title>
+</head>
+<body>
+<img src="../img/gio.webp" width="100px" id="img" onmouseout="appear()" onmouseover="dissappear()">
+
+
+    <script>
+        function dissappear(){
+        document.getElementById('img').style.display = "none";
+    }
+
+        function appear(){
+        document.getElementById('img').style.display = "block";
+    }
+    </script>
+</body>
+</html>
+
 /* Put code for the task below */
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,21 @@
 /* Put code for the task below */
+<button type="button" onClick="backgroundChange()">Click Me</button>
+
+<script>
+    //document.getElementById("table").style.backgroundColor = "blue";
+
+    // Access to the HTML Element
+    table = document.getElementById("table");
+    counter = 0;
+
+    function backgroundChange(){
+    if (counter % 2 === 0){
+    table.style.backgroundColor = "red";
+}else{
+    table.style.backgroundColor = "blue";
+}
+    counter++;
+
+}
+
+</script>
